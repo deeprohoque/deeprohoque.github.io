fceafdd0665553e6b0db85c9973dbebdc7dffff5
Deepro Hoque <deeprohoque@gmail.com>
2023-03-09 09:18:23 -0800

    created vite react website and added dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      its a me
+    </div>
+  )
+}
+
+export default App
